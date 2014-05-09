fe93fa0f977fee6204536ca851607ef6f0b674f8
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-09 16:45:39 -0400

    Concatenation
    Combine text strings

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -130,5 +130,24 @@ var width = 6;
 
 var perimeter = (length * 2) + (width *2);
 console.log(perimeter);
+console.log("The Perimeter of a rectangle with a length of"+length+" and a width of "+width+" is" +perimeter+".");
 
+
+//Concatenating - combining strings of text together
+
+var firstName = "Kermit";
+var lastName = "The Frog";
+
+//Combine the first and last into a full name
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var d = "6";
+
+var e = "7";
+
+var combine = d+e;
+console.log(combine);
 

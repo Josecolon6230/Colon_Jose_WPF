35a5c3ecabe1dfc9eee094b0c694ebf0c754e919
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-09 15:32:54 -0400

    Area of Triangle
    Coded how to find the area of the Triangle

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -17,3 +17,21 @@ var  birthYear = 1991;
 
 var age = 2014 - birthYear - 1;
 console.log(age);
+
+
+//Math
+// + - / *
+
+
+
+//Find the area of triangle
+//Base * height * 1/2
+
+var base = 4;
+var height = 5;
+
+
+
+var areaTriangle = base * height * .5;
+
+console.log(areaTriangle);

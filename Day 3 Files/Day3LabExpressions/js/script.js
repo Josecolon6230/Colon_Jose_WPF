aa7ba6851d450fc9901d78e0b389d6cc2c238616
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-09 17:51:01 -0400

    Slice of Pie 2
    Completed Slice of pie 2

--- Day 3 Files/Day3LabExpressions/js/script.js
+++ Day 3 Files/Day3LabExpressions/js/script.js
@@ -21,3 +21,22 @@ var pizzaPerPerson = slices * pizzaBoxes / people;
 
 console.log("Each person ate "+pizzaPerPerson+" slices of pizza at the party.");
 
+
+//Slice of Pie 2
+
+//Calculate how many slices of pizza Sparky got
+
+
+// Use % to find out the amount of slices Sparky gets using the Variables from Slice of Pie 1
+
+
+var sparky = slices * pizzaBoxes % people;
+
+console.log("Sparky got "+sparky+" slices of pizza.")
+
+
+
+//Average shopping bill
+
+//Find out the total amount of money spent in 5 weeks for grocery and its average
+

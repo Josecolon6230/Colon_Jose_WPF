f41f88796db9c1c0ce64c47caf8a8d9f92374c21
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-09 15:25:16 -0400

    Found age on JS
    Add JS file to show age

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -5,4 +5,15 @@ Day 3 Class Examples
  */
 
 
-alert("Your page is linked correctly");
+//alert("Your page is linked correctly");
+
+
+//Find out how old we are
+//Create a variable for birth year
+var  birthYear = 1991;
+
+
+//Create a variable to hold the age
+
+var age = 2014 - birthYear - 1;
+console.log(age);

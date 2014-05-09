d0bba09db286f357fd25fffb626573859382f93d
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-09 17:26:45 -0400

    Slice of Pie 1
    Finished the first question

--- Day 3 Files/Day3LabExpressions/js/script.js
+++ Day 3 Files/Day3LabExpressions/js/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by Dusk on 5/9/14.
+/*
+Jose Colon
+WPF 1405
+Day 3 Lab Expressions
  */
+
+
+//Slice of Pie 1
+
+// Calculate how many slices of Pizza each person ate
+
+var slices = 10;
+
+var people = 6;
+
+var pizzaBoxes = 4;
+
+//Multiply slices * pizzaBoxes / 6
+
+var pizzaPerPerson = slices * pizzaBoxes / people;
+
+console.log("Each person ate "+pizzaPerPerson+" slices of pizza at the party.");
+

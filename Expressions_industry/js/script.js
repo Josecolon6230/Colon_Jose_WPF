dfc212aeb215a1de21c19e2ca5ca1b6a145f77f6
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-12 19:36:53 -0400

    Expressions Industry
    Completed this part of the Homework with a fully working and checked
    code

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -4,6 +4,30 @@ WPF Section 1
 Expressions Industry
  */
 
-alert("testing");
+//alert("testing");
 
+//Going to make a array that will give the average number of hours work with a average amount of hours worked on the website
+
+
+
+var hoursWorked = [80, 65, 120, 70];
+
+// get the total hours worked
+var totalHours = hoursWorked[0] + hoursWorked[1] + hoursWorked[2] + hoursWorked[3];
+
+//Testing
+console.log(totalHours); // working
+
+//Finding the average hours worked
+
+var averageHours = totalHours / hoursWorked.length;
+
+//testing
+console.log(averageHours); //works
+
+
+//Result
+var result = ("The completed website took a total of " + totalHours + " hours, so the website was worked on for a average of " + averageHours + " hours.");
+
+console.log(result); //working and completed
 

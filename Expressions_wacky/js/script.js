da47cc0839ef71cb6032b539b3c63c4245e8a1b6
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-12 19:58:50 -0400

    Expressions Wacky
    Very wacky expressions that does not use much math but is funny and
    works

---
+++ Expressions_wacky/js/script.js
@@ -0,0 +1,32 @@
+/*
+jose Colon
+WPF Section 1
+Expressions Wacky
+ */
+
+
+//alert("Testing");  //working
+
+
+//WHEN SQUIRRELS ATTACK!
+
+var howMany = prompt("How many squirrels did you see today?");
+
+var yourHeight = prompt("How are tall are you?");
+
+var squirrelsToDeath = howMany*yourHeight;
+
+
+//testing
+console.log(squirrelsToDeath); //Working
+
+//result
+
+var result = ("You saw " + howMany + " squirrels today, So guess what...." + squirrelsToDeath + " Squirrels are going\n to kill you...GOOD LUCK");
+
+
+//testing
+console.log(result);
+alert(result);
+
+

c624555eab409847e6b0a9afa6c4da6e4cae850b
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-12 16:15:20 -0400

    Prompts Lesson
    Learned about prompts

---
+++ Day 4 files/js/day4_prompts.js
@@ -0,0 +1,34 @@
+/*
+Jose Colon
+WPF Section 1
+Day 4 Prompts
+ */
+
+//alert("Testing");
+
+//Prompting user for information
+//prompt("Enter your year of birth:");
+
+//Capture the information the user provides
+
+//var userInput = prompt("Enter your birth year:");
+//console.log(userInput);
+
+//Prompt and explain to the user what we are actually doing.
+//Prompt the user for a width and calculate the area a rectangle with it
+
+var width = prompt("Let's calculate the area of a rectangle. \nPlease enter a width");
+var height = prompt("Please enter a height:")
+
+//area of rectangle - width * height
+
+var area =width*height;
+
+console.log("The area of your rectangle is "+area);
+
+//Create a variable to hold the whole string of an answer
+
+var result = "The area of a rectangle with a width of "+ width + " and a height of " + height + " is " + area + ".";
+console.log(result);
+alert(result);
+

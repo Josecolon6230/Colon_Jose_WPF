5dec689f58a80b0524c946fd399b14b621da3109
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-14 16:30:16 -0400

    Ternary basic IF statement
    Basics of if statement with ternary

---
+++ Day 5 files/js/day5_Ternary.js
@@ -0,0 +1,18 @@
+/*
+Jose Colon
+WPF Section 1
+Ternary Conditionals
+ */
+//alert("Testing");
+
+
+//Check our GPA and see if we can graduate
+//Higher 2.0 GPA to graduate
+
+var gpa = 3.7;
+
+if(gpa > 2.0){
+    console.log("congrats! You can graduate!");
+}else{
+    console.log("Sorry your GPA is too low");
+}

4c4272bef65e7a94fc52b8066fb922f3c3b409dd
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-14 15:01:40 -0400

    Else if statements
    Will check multiple conditions and run one block of code

--- Day 5 files/js/day5_Conditionals.js
+++ Day 5 files/js/day5_Conditionals.js
@@ -10,12 +10,17 @@ Conditionals Exercise
 //Relational Expression
 
 
-//If the child is not over 48" tall, then he can't ride
+//If the child is 48 or taller he can ride the coaster
+//If the child is riding with a parent, then he can be 45" tall. Anything smaller than that, no riding
 
-
-var kidHeight = 50;
+var kidHeight = 45;
 
 var minHeight = 48;
+
+var wParentHeight = 45;
+
+
+
 
 /*
 if(thing to evaluate){
@@ -26,6 +31,9 @@ if(thing to evaluate){
 if(kidHeight >= minHeight){
     //code will run if the kid is tall enough
     console.log("You can ride the roller coaster");
+} else if(kidHeight >= wParentHeight){
+    console.log("You can ride with a parent!");
+
 } else {
     //Code will run if the kid is not tall enough
     console.log("Sorry kid you are not tall enough");

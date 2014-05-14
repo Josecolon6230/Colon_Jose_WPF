924f498c29b8dc4df9bffcfd275e178eed0ea0f4
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-14 13:58:29 -0400

    Basic if statements
    Learned about if statements

---
+++ Day 5 files/js/day5_Conditionals.js
@@ -0,0 +1,29 @@
+/*
+Jose Colon
+WPF Section 1
+Conditionals Exercise
+ */
+
+//alert("Testing");
+
+
+//Basic Conditional - If Statement
+
+
+//If the child is tall enough, print to the console that he can ride a roller coaster
+
+
+var tallEnough = false;
+
+/*
+if(thing to evaluate){
+    code to run if it is true!
+}
+ */
+
+if(tallEnough){
+    //code will run if the kid is tall enough
+    console.log("You can ride the roller coaster");
+}
+
+console.log("What comes after");

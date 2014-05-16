0eff2df80a35ed44c64bad80f4c7e5f9793b2737
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-16 15:23:55 -0400

    Math class is in session
    learning about rounding, floor and cidl

---
+++ Day 6 files/js/day6_Math.js
@@ -0,0 +1,30 @@
+/*
+Jose Colon
+WPF Section 1
+Math Class
+ */
+
+//alert("Testing");
+
+//round()
+//Normal rounding .5 goes up, less goes down
+
+var num1 = 9.444;
+console.log(num1);
+console.log(Math.round(num1));
+
+
+//Math.floor()
+//floor chops off all decimals and always rounds down.
+var num2 = 6.9;
+console.log(num2);
+console.log(Math.floor(num2));
+
+
+//Math.ceil()
+//Always rounds up
+
+
+var num3 = 4.8;
+console.log(num3);
+console.log(Math.ceil(num3));

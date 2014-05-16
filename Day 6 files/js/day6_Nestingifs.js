4ad8b468d7a8c2ac79a679f183dfab120c031ae3
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-16 14:52:37 -0400

    Nesting If statement set up
    This is not correct this is the basic set up

---
+++ Day 6 files/js/day6_Nestingifs.js
@@ -0,0 +1,35 @@
+/*
+Jose Colon
+WPF Section 1
+Nesting If Statements
+ */
+//alert("Testing");
+
+//Some Decisions affect other decisions
+
+
+//If it is warm outside, lets go to the beach.
+//If its not....lets go to the movies!
+
+//If the water is warm, I'm going in the water
+//If the water is not warm, I a going to get a tan
+
+//Create variable for outside temp
+var temp = 90; // degrees
+
+//Water temp
+var waterTemp = 78; //degrees
+
+if(temp>=85){
+    console.log("Let's go to the beach");
+}else{
+    console.log("Let's go see a movie");
+}
+
+
+
+if(waterTemp>=75){
+    console.log("Let's get in the water!");
+}else{
+    console.log("Let's get a tan!");
+}

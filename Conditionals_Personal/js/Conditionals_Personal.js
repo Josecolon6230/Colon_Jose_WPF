8b9a4c15032b995b89424f3e7aee2a6a92f9c1ac
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-16 17:58:18 -0400

    Conditionals personal
    testing a few things before i continue to the next  Conditionals

--- Conditionals_Personal/js/Conditionals_Personal.js
+++ Conditionals_Personal/js/Conditionals_Personal.js
@@ -9,6 +9,29 @@ Conditionals Personal
 
 //Want to buy a Xbox One
 //Going to find out if I have enough for a Xbox One or not
+//Price of Xbox One $400
+//Price of Xbox One with Kinect $500
+//Price of Xbox One with Titanfall bundle $500 but will add 100 dollars to make sure code works
 
 
+
+var xbox = 400; //dollars
+
+var moneyHave = Number(prompt("How much money do you have? (Ex: 10.87)"));
+
+var amountNeeded = xbox - moneyHave; // will be used if still missing amount for xbox
+
+//(moneyHave >= xbox) ? console.log("Go to the nearest store and get yourself a Xbox One!"): console.log("You are still missing $"+amountNeeded+" better save up for a little longer.");
+
+
+
+if(moneyHave>xbox){
+    console.log("Go to the nearest store and get yourself a Xbox One!");
+}else if(moneyHave>=xboxKinect){
+    console.log("Go to the nearest store and get yourself a Xbox One Kinect bundle!");
+}else if(moneyHave>=xboxBundle){
+    console.log("Go to the nearest store and get yourself a Xbox One Bundle with TitanFall!");
+}else{
+    console.log("You can't get one yet, but keep on saving");
+}
 

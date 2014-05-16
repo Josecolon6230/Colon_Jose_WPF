2d830d84977f04372bbb9ff9647404c716a81911
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-16 18:18:20 -0400

    Conditionals Personal
    tried two different versions and they both seem to work fine

--- Conditionals_Personal/js/Conditionals_Personal.js
+++ Conditionals_Personal/js/Conditionals_Personal.js
@@ -15,23 +15,28 @@ Conditionals Personal
 
 
 
-var xbox = 400; //dollars
 
 var moneyHave = Number(prompt("How much money do you have? (Ex: 10.87)"));
 
-var amountNeeded = xbox - moneyHave; // will be used if still missing amount for xbox
+var amountNeeded = 400 - moneyHave; // will be used if still missing amount for xbox
 
-//(moneyHave >= xbox) ? console.log("Go to the nearest store and get yourself a Xbox One!"): console.log("You are still missing $"+amountNeeded+" better save up for a little longer.");
+var xbox = 400;
+
+var forKinect = amountNeeded + 100;
+
+var forTitan = amountNeeded + 200;
 
 
+(moneyHave >= xbox) ? console.log("Go to the nearest store and get yourself a Xbox One!"): console.log("You are still missing $"+amountNeeded+" better save up for a little longer.");
 
-if(moneyHave>xbox){
-    console.log("Go to the nearest store and get yourself a Xbox One!");
-}else if(moneyHave>=xboxKinect){
-    console.log("Go to the nearest store and get yourself a Xbox One Kinect bundle!");
-}else if(moneyHave>=xboxBundle){
-    console.log("Go to the nearest store and get yourself a Xbox One Bundle with TitanFall!");
+
+if(moneyHave<400){
+    console.log("You are still missing $"+amountNeeded+" for a Xbox One, and if you want Kinect you will need $"+forKinect+" or $"+forTitan+" for the the Titanfall bundle.");
+}else if(moneyHave<500){
+    console.log("You can get your Xbox One! what are you waiting for?!");
+}else if(moneyHave<600){
+    console.log("You can get your Xbox One Kinect bundle! Get to the nearest store!");
 }else{
-    console.log("You can't get one yet, but keep on saving");
+    console.log("You can get your Xbox One TitanFall Bundle, Hurry up and get into the Combat!");
 }
 

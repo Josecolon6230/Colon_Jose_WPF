0cc911e8b42d0fc9e32f5205eb1d813e39c9f28d
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-19 14:49:59 -0400

    For loop
    workhorse of the loops, Good for when you know how many times to run it

---
+++ Day 7 files/js/day7_forLoops.js
@@ -0,0 +1,17 @@
+/*
+Jose Colon
+WPF Section 1
+For Loops
+ */
+
+//alert("Testing");
+
+//Basic for loop
+
+//for(initialization; Condition to test ; change variable)
+
+for(var i=1; i<20; i*=2){
+
+    console.log("i = "+i);
+
+}

1e23be289657416601b6ea0fbf83ebb2bf75eeaf
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-19 15:05:05 -0400

    breaks
    stopping loops in its tracks

--- Day 7 files/js/day7_forLoops.js
+++ Day 7 files/js/day7_forLoops.js
@@ -15,3 +15,18 @@ for(var i=1; i<20; i*=2){
     console.log("i = "+i);
 
 }
+
+
+//Breaks
+//break - stops the loop anywhere it is
+
+
+for(var k=0; k<5; k++){
+
+    console.log("k = "+k);
+
+    if(k===3){
+        break;
+    }
+
+}

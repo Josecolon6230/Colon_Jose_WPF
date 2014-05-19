8ddfff740a710219a8ca76f7ded8646fa4eaaeaf
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-19 14:05:09 -0400

    Basic While Loop
    Created a basic while loop

--- Day 7 files/js/day7_WhileLoops.js
+++ Day 7 files/js/day7_WhileLoops.js
@@ -4,4 +4,28 @@ WPF Section 1
 While Loops
  */
 
-alert("Testing");
+//alert("Testing");
+
+
+//Basic While Loop
+
+//While loop. loops through a block of code as long as it is true
+
+//Initialize the counter variable
+
+var counter = 0;
+
+//Setup while loop
+
+while(counter<200){ //loops until false
+    console.log("Something just never ends");
+    //change the value of the counter variable
+    //or it will run forever! not good!
+    console.log("The counter is "+counter);
+    counter++;
+}
+
+
+console.log("Ending counter "+counter);
+
+

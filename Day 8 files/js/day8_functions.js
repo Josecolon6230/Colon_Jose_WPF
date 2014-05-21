a5b4e490c9992c523a6aeab7f004fe31f400bc78
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-21 15:13:13 -0400

    Very Basic of a Function
    PrintHello

---
+++ Day 8 files/js/day8_functions.js
@@ -0,0 +1,24 @@
+/*
+Jose Colon
+WPF Section 1
+Functions
+ */
+//alert("Testing");
+
+
+//Basic function
+//function functionName(){}
+
+//Create a function that console.log hello
+
+function printHello(){
+
+    console.log("Hello!");
+}
+
+//function call our printHello Function
+//"Go button" or "On Switch"
+
+//functionName()
+
+printHello(); //function call or invocation

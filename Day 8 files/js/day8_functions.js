07acd2115f794b970c9347e8f7a57677419e7782
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-21 15:21:04 -0400

    Create 3 functions
    Create 3 functions

--- Day 8 files/js/day8_functions.js
+++ Day 8 files/js/day8_functions.js
@@ -16,9 +16,49 @@ function printHello(){
     console.log("Hello!");
 }
 
+
+
+
+//Create a function that console.logs "prints out more"
+
+function printMore(){
+
+    console.log("Prints out more Text!");
+}
+
+
+//function call calcArea
+calcArea(); // functions can be called any where in the code
+
+
 //function call our printHello Function
 //"Go button" or "On Switch"
 
 //functionName()
 
 printHello(); //function call or invocation
+
+printMore();
+
+printHello();
+
+
+printMore();
+
+
+//create a function that calculates the area of a rectangle
+
+function calcArea(){
+
+    //create variable for width, height, and area
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+
+    //print out the area
+    console.log(area);
+
+
+}
+
+calcArea();

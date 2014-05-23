10bb5a6b6b42b62ddcf2ad941fb92c6298140fc8
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-23 15:31:15 -0400

    calculate using multiple functions
    how to calculate using more than one functions

--- Day 9 files/js/day9_Multiple.js
+++ Day 9 files/js/day9_Multiple.js
@@ -3,6 +3,68 @@
 Jose Colon
 WPF May 2014
 Section 1
+Multiple Functions
  */
 
-alert("Testing");
+//alert("Testing");
+
+
+ //Prompt the user for width and height
+ var width = Number(prompt("Please type in a Rect width:"));
+ var height = Number(prompt("Please type in a Rect Height:"));
+
+ //Create a fucntion call to perimeter
+
+var perimeter = perimeterRect(width, height);
+ console.log(perimeter);
+
+
+
+ //Function Call to find out the area
+ var area = areaRectangle(width, height);
+ console.log(area);
+
+ //Final console.log
+
+ console.log("A rectangle with a width of "+width+" and a height of "+height+" has a perimeter of "+perimeter+" and a area of "+area+".");
+ alert("A rectangle with a width of "+width+" and a height of "+height+" has a perimeter of "+perimeter+" and a area of "+area+".");
+
+
+
+
+
+ //Create a function to find a perimeter of a rectangle
+
+ function perimeterRect(width, height){
+
+
+     //Perimeter of rect is
+     //2*width + 2*height
+     var p =2*width + 2*height;
+
+
+     //Return the perimeter
+     return p;
+
+
+
+
+ }
+
+
+
+
+
+ function areaRectangle(w, h){
+
+
+     //Area of a rectangle w*h
+
+     var a = w*h;
+     //Return the area
+     return a;
+
+
+
+
+ }

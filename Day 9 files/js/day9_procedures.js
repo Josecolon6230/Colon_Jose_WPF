2bc122580d9a3f3f53ea80266f093dffb7eb4272
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-23 14:40:49 -0400

    how to do procedures
    procedure functions

---
+++ Day 9 files/js/day9_procedures.js
@@ -0,0 +1,39 @@
+
+ /*
+Jose Colon
+WPF May 2014
+Section 1
+Procedures
+ */
+
+//alert("Testing");
+
+
+
+//Basic FUNCTION
+
+ function calcAreaF(){
+     var width = 6;
+     var height = 7;
+     var area = width*height;
+     return area;
+
+
+
+
+
+ }
+
+ //Basic procedure
+ //List of steps to do
+ //NEVER returns a value
+
+ function calcAreaP(){
+     var width = 5;
+     var height = 6;
+     var area = width*height;
+
+     console.log(area);
+
+
+ }

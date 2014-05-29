b6f7a5547729eaff55fb3d1277de0e9280f3efbf
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-28 22:40:15 -0400

    Functions Industry
    Finished a random code for this hw

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -3,6 +3,24 @@
 Jose Colon
 WPF May 2014
 Section 1
+Functions Industry
  */
 
-alert("Testing");
+//alert("Testing");
+
+
+ //Gonna make random codes for anything that comes into mind
+
+
+ //Riot voting on skins
+ alert("You got a new skin idea for the League of Legends game\n Input how many people approve of your new skin idea");
+
+ var skinApproved = Number(prompt("How many employees voted on the new skin idea?"));
+
+ (skinApproved>30) ? console.log("Start working on the new skin!!"): console.log("Hmmm seems like you might want to think of a new skin idea");
+
+ while (isNaN(skinApproved) || skinApproved===""){
+
+     skinApproved = Number(prompt("Please enter the amount of employees who approved the new skin idea:"));
+
+ }

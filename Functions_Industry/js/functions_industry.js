6e8ddd2e1156f68e81102889a6881294878114fc
Josecolon6230 <josecolon6230@fullsail.edu>
2014-05-28 22:23:37 -0400

    Functions personal
    made a anonymous function that finds the volume of a cylinder and i
    should make more for other shapes may help in the future

---
+++ Functions_Industry/js/functions_industry.js
@@ -0,0 +1,8 @@
+
+ /*
+Jose Colon
+WPF May 2014
+Section 1
+ */
+
+alert("Testing");
---
+++ Functions_Personal/js/functions_personal.js
@@ -0,0 +1,31 @@
+
+ /*
+Jose Colon
+WPF May 2014
+Section 1
+ */
+
+//alert("Testing");
+
+
+ //Help me find the Volume of a cylinder
+
+
+ //Volume for cylinder is V = PI * r*r * h
+
+
+ var volumeCylinder = function(r ,h){
+
+
+
+     volume = Math.PI*r*r*h;
+
+     return volume;
+
+
+
+ }
+
+var volCyli = volumeCylinder(5,10);
+
+ console.log("The volume of your cylinder is "+volCyli);
